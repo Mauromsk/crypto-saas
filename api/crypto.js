5516607e00273d266994df338d1de91f521cda99
Mauro Mosca <mauromma1234@gmail.com>
2026-03-10 19:03:19 -0300

    remove vercel1

--- api/crypto.js
+++ api/crypto.js
@@ -43,3 +43,12 @@ export default async function handler(req, res) {
   }
 
 }
+export default function handler(req, res) {
+  res.status(200).json({
+    BTC: 65000,
+    ETH: 3200,
+    SOL: 150
+  });
+}
+
+
