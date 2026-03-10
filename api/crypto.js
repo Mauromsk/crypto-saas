434f38683fc22eb9a4bf102b128d3e106cc056fc
Mauromsk <mauromma1234@gmail.com>
2026-03-10 18:29:13 -0300

    Update crypto.js

--- api/crypto.js
+++ api/crypto.js
@@ -1,20 +1,7 @@
-export default async function handler(req, res) {
-
-  const coin = req.query.coin || "bitcoin";
-
-  const url = `https://api.coingecko.com/api/v3/coins/${coin}/market_chart?vs_currency=usd&days=1`;
-
-  const response = await fetch(url);
-  const data = await response.json();
-
+export default function handler(req, res) {
   res.status(200).json({
-    coin: coin,
-    prices: data.prices
-  });
-
-}
-export default async function handler(req, res) {
-  res.status(200).json({
-    message: "API funcionando"
+    status: "API funcionando",
+    crypto: "bitcoin",
+    price: 65000
   });
 }
