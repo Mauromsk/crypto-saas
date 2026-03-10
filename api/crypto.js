9bd65b72952eab10c727169b6b1908ce8847fb01
Mauro Mosca <mauromma1234@gmail.com>
2026-03-10 18:11:53 -0300

    update api

--- api/crypto.js
+++ api/crypto.js
@@ -1,25 +1,15 @@
 export default async function handler(req, res) {
 
-  try {
+  const coin = req.query.coin || "bitcoin";
 
-    const coin = req.query.coin || "bitcoin";
+  const url = `https://api.coingecko.com/api/v3/coins/${coin}/market_chart?vs_currency=usd&days=1`;
 
-    const url = `https://api.coingecko.com/api/v3/coins/${coin}/market_chart?vs_currency=usd&days=1`;
+  const response = await fetch(url);
+  const data = await response.json();
 
-    const response = await fetch(url);
-    const data = await response.json();
-
-    res.status(200).json({
-      coin: coin,
-      prices: data.prices
-    });
-
-  } catch (error) {
-
-    res.status(500).json({
-      error: "Error fetching crypto data"
-    });
-
-  }
+  res.status(200).json({
+    coin: coin,
+    prices: data.prices
+  });
 
 }
